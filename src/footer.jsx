1b07be3f56635fe9bc2cb0ec2554b592f1529e95
Mammajam <jammamadou@gmail.com>
2025-02-25 10:01:22 +0000

    Changed the year in my footer component to be dynamic as per Javascript date object.

--- src/footer.jsx
+++ src/footer.jsx
@@ -3,12 +3,14 @@ import { FaInstagram, FaWhatsapp } from 'react-icons/fa'
 import { LuMailPlus } from "react-icons/lu"
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-gray-100 py-6">
       <div className="container mx-auto px-4">
         <div className="md:flex md:justify-around items-center">
           <div className="text-gray-600 text-center md:text-left mb-4 md:mb-0">
-            © 2025 Jam Dev. All rights reserved.
+            © {currentYear} Jam Dev. All rights reserved.
           </div>
           <div className="flex justify-center space-x-4">
             <a href="https://www.instagram.com/mammajam_" className="text-blue-500 hover:text-blue-700">
